c2e55d013478622c955d2b621925aa1c3c5eee5d
Juangui Jordán <jjordan@MacBook-Pro-de-Juangui.local>
2020-10-04 12:17:58 +0200

    Added axios and simplified api methods

--- src/modules/customer/customer.api.ts
+++ src/modules/customer/customer.api.ts
@@ -1,40 +1,20 @@
+import axios from 'axios';
 import { OrderType } from '../shared/shared.types';
 import { Customer } from './customer.types';
 
-const loadCustomers = (page: number, limit: number, order: OrderType): Promise<Customer[]> => {
-  return fetch(`/customers?_page=${page}&_limit=${limit}&_sort=name&_order=${order}`).then((response) =>
-    response.json()
-  );
-};
+const loadCustomers = (page: number, limit: number, order: OrderType): Promise<Customer[]> =>
+  axios.get<Customer[]>(`/customers?_page=${page}&_limit=${limit}&_sort=name&_order=${order}`).then(({ data }) => data);
 
-const loadCustomer = (customerId: number): Promise<Customer> => {
-  return fetch(`/customers/${customerId}`).then((response) => response.json());
-};
+const loadCustomer = (customerId: number): Promise<Customer> =>
+  axios.get(`/customers/${customerId}`).then(({ data }) => data);
 
-const saveCustomer = (customer: Customer): Promise<number> => {
-  return fetch('/customers', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(customer),
-  }).then((response) => response.json());
-};
+const saveCustomer = (customer: Customer): Promise<number> =>
+  axios.post('/customers', customer).then(({ data }) => data);
 
-const updateCustomer = (customer: Customer): Promise<number> => {
-  return fetch(`/customers/${customer.id}/`, {
-    method: 'put',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(customer),
-  }).then((response) => response.json());
-};
+const updateCustomer = (customer: Customer): Promise<number> =>
+  axios.put(`/customers/${customer.id}/`, customer).then(({ data }) => data);
 
-const deleteCustomer = (customerId: number): Promise<void> => {
-  return fetch(`/customers/${customerId}`, {
-    method: 'delete',
-  }).then(() => undefined);
-};
+const deleteCustomer = (customerId: number): Promise<void> =>
+  axios.delete(`/customers/${customerId}`).then(() => undefined);
 
 export const customerApi = { deleteCustomer, loadCustomer, loadCustomers, saveCustomer, updateCustomer };
--- src/modules/product/product.api.ts
+++ src/modules/product/product.api.ts
@@ -1,40 +1,19 @@
+import axios from 'axios';
 import { OrderType } from '../shared/shared.types';
 import { Product } from './product.types';
 
-const loadProducts = (page: number, limit: number, order: OrderType): Promise<Product[]> => {
-  return fetch(`/products?_page=${page}&_limit=${limit}&_sort=name&_order=${order}`).then((response) =>
-    response.json()
-  );
-};
+const loadProducts = (page: number, limit: number, order: OrderType): Promise<Product[]> =>
+  axios.get<Product[]>(`/products?_page=${page}&_limit=${limit}&_sort=name&_order=${order}`).then(({ data }) => data);
 
-const loadProduct = (productId: number): Promise<Product> => {
-  return fetch(`/products/${productId}`).then((response) => response.json());
-};
+const loadProduct = (productId: number): Promise<Product> =>
+  axios.get(`/products/${productId}`).then(({ data }) => data);
 
-const saveProduct = (product: Product): Promise<number> => {
-  return fetch('/products', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(product),
-  }).then((response) => response.json());
-};
+const saveProduct = (product: Product): Promise<number> => axios.post('/products', product).then(({ data }) => data);
 
-const updateProduct = (product: Product): Promise<number> => {
-  return fetch(`/products/${product.id}/`, {
-    method: 'put',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(product),
-  }).then((response) => response.json());
-};
+const updateProduct = (product: Product): Promise<number> =>
+  axios.put(`/products/${product.id}/`, product).then(({ data }) => data);
 
-const deleteProduct = (productId: number): Promise<void> => {
-  return fetch(`/products/${productId}`, {
-    method: 'delete',
-  }).then(() => undefined);
-};
+const deleteProduct = (productId: number): Promise<void> =>
+  axios.delete(`/products/${productId}`).then(() => undefined);
 
 export const productApi = { deleteProduct, loadProduct, loadProducts, saveProduct, updateProduct };
