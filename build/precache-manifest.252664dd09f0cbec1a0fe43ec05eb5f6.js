d84871543ae8902a9dd93001e5098ae5f65d56f0
_jordaj3 <Madridcapital2020>
2020-11-18 02:57:04 +0100

    Add static build for heroku

---
+++ build/precache-manifest.252664dd09f0cbec1a0fe43ec05eb5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b918b162c654b53c35084b2a6f052e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1dcab2746f59b995760",
+    "url": "/static/css/main.3168e372.chunk.css"
+  },
+  {
+    "revision": "77c5781821c77b49cc3e",
+    "url": "/static/js/2.cfc077b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfc077b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1dcab2746f59b995760",
+    "url": "/static/js/main.163eda8d.chunk.js"
+  },
+  {
+    "revision": "17c3660b4fb5149def29",
+    "url": "/static/js/runtime-main.42df2998.js"
+  }
+]);
