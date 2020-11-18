9c24bb00770889f9b84a66b76dfc55344f51bed0
_jordaj3 <Madridcapital2020>
2020-11-18 03:38:50 +0100

    Fixed json server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('src/db/db.json');
 const middlewares = jsonServer.defaults({static: './build'});
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
