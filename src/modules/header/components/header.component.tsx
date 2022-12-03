9d89e1b6013cdc5e768ee8e825c5160a51a62440
_jordaj3 <juangui.jordan2@takeda.com>
2022-12-03 20:07:56 +0100

    Added header navigation color to selected item

--- src/modules/header/components/header.component.tsx
+++ src/modules/header/components/header.component.tsx
@@ -1,29 +1,33 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { ApplicationStore } from '../../../store/store';
 import { authCommands } from '../../auth/auth.commands';
 import './header.component.scss';
 
 export const BOHeader: FC = () => {
   const history = useHistory();
+  const location = useLocation();
+
   const isAuthenticated = useSelector<ApplicationStore, boolean>((state) => state.ui.auth.isAuthenticated);
 
   const logout = () => {
     authCommands.logout().then(() => history.push('/login'));
   };
 
+  const isCurrentPath = (path: string) => path === location.pathname;
+
   return isAuthenticated ? (
     <div className="header">
       <div className="headerTitle">backoffice</div>
       <div className="headerNavigation">
         <div className="headerNavigationItem">
-          <Link className="buttonLink" to="/customers">
+          <Link className={`buttonLink${isCurrentPath('/customers') ? ' selected' : ''}`} to="/customers">
             Customers
           </Link>
         </div>
         <div className="headerNavigationItem">
-          <Link className="buttonLink" to="/products">
+          <Link className={`buttonLink${isCurrentPath('/products') ? ' selected' : ''}`} to="/products">
             Products
           </Link>
         </div>
