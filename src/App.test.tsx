774cdde7b9118e0ab3d7d524a0902ef3abb557b1
_jordaj3 <juangui.jordan2@takeda.com>
2022-12-04 09:16:56 +0100

    Added component tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('should render login page by default', () => {
-  render(<App />);
+describe('App', () => {
+  it('should render login page by default', () => {
+    render(<App />);
 
-  expect(screen.getByText('backoffice')).toBeInTheDocument();
-  expect(screen.getByText('Start Session')).toBeInTheDocument();
-  expect(screen.getByText('Email')).toBeInTheDocument();
-  expect(screen.getByText('Password')).toBeInTheDocument();
-  expect(screen.getByRole('button').textContent).toBe('Login');
+    expect(screen.getByText('backoffice')).toBeInTheDocument();
+    expect(screen.getByText('Start Session')).toBeInTheDocument();
+    expect(screen.getByText('Email')).toBeInTheDocument();
+    expect(screen.getByText('Password')).toBeInTheDocument();
+    expect(screen.getByRole('button').textContent).toBe('Login');
+  });
 });
